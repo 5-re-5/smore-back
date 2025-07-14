b5a150705eb9fc6079bd31fc6cde5c4fa9a38924
DESKTOP-HA97GMI\SSAFY <kilhyunii@naver.com>
2025-07-14 13:39:49 +0900

    Initial commit

---
+++ src/test/java/org/oreo/smore/SmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oreo.smore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
