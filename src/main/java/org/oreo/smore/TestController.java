5966043a10188bcc54d8e95abfe1d841018d7a81
KiminSon <kimin0331@naver.com>
2025-07-22 13:06:07 +0900

    feat: 테스트용 api 추가

---
+++ src/main/java/org/oreo/smore/TestController.java
@@ -0,0 +1,12 @@
+package org.oreo.smore;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "1단계 배포 성공!";
+    }
+}
