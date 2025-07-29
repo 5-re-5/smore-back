525e8ce71d1c16555cc58bd0c64189ae76ceb600
hongjungin <jungini0000@gmail.com>
2025-07-29 15:18:15 +0900

    test: OpenVidu 설정 테스트

---
+++ src/test/java/org/oreo/smore/config/OpenViduConfigTest.java
@@ -0,0 +1,30 @@
+package org.oreo.smore.config;
+
+import io.openvidu.java.client.OpenVidu;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "OPENVIDU_URL=https://172.26.3.213:4443",
+        "OPENVIDU_SECRET=jD8fK4qPw1x_2VzLsRm9YeTnA0UcB3zWd7oKiXJ6NvQpGtM5EbChZrLjy"
+})
+class OpenViduConfigTest {
+
+    @Autowired
+    private OpenVidu openVidu;
+
+    @Test
+    void openViduBean이_정상적으로_생성된다() {
+        assertThat(openVidu).isNotNull();
+    }
+
+    @Test
+    void openViduUrl이_올바른_타입이다() {
+        assertThat(openVidu).isInstanceOf(OpenVidu.class);
+    }
+}
