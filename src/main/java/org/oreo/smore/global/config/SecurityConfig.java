b5aa2de89a40a76aae9f5559f7f628ef38a01580
DESKTOP-HA97GMI\SSAFY <kilhyunii@naver.com>
2025-08-02 14:55:51 +0900

    fix: 기본 로그인 경로 띄워주는 오류 수정
    문제: 백엔드 url 접속 시, 권한 없으면 기본 로그인 경로로 이동
    원인: Spring Security 에서 기본 로그인 경로 이동 안막음
    해결: 권한 없을 시, 401을 띄워주도록 handler를 filter chain 에 추가

--- src/main/java/org/oreo/smore/global/config/SecurityConfig.java
+++ src/main/java/org/oreo/smore/global/config/SecurityConfig.java
@@ -8,10 +8,12 @@ import org.oreo.smore.domain.auth.token.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -26,11 +28,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, TokenService tokenService) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/auth/**", "/oauth2/**").permitAll()
                         .anyRequest().authenticated()
                 )
-
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(u -> u.userService(oAuth2UserService))
                         .successHandler(new OAuth2SuccessHandler(tokenProvider, tokenService))
