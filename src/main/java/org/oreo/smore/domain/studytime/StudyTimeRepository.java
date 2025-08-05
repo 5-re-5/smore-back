426a90633ca6121596976136008947f60cbfa2af
KiminSon <kimin0331@naver.com>
2025-08-05 15:52:19 +0900

    feat: 공부 시간 업데이트 구현

--- src/main/java/org/oreo/smore/domain/studytime/StudyTimeRepository.java
+++ src/main/java/org/oreo/smore/domain/studytime/StudyTimeRepository.java
@@ -3,6 +3,9 @@ package org.oreo.smore.domain.studytime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudyTimeRepository extends JpaRepository<StudyTime, Long> {
+    Optional<StudyTime> findTopByUserIdOrderByCreatedAtDesc(Long userId);
 }
--- src/main/java/org/oreo/smore/domain/studytime/StudyTimeService.java
+++ src/main/java/org/oreo/smore/domain/studytime/StudyTimeService.java
@@ -14,17 +14,21 @@ public class StudyTimeService {
     /**
      * 공부 시작
      */
-    public StudyTime startStudyTime(Long userId) {
+    public void startStudyTime(Long userId) {
         StudyTime studyTime = StudyTime.builder()
                 .userId(userId)
                 .createdAt(LocalDateTime.now()) // 서버에서 현재 시간 저장
                 .deletedAt(LocalDateTime.now()) // 서버에서 현재 시간 저장
                 .build();
 
-        return studyTimeRepository.save(studyTime);
+        studyTimeRepository.save(studyTime);
     }
 
     public void updateStudyTime(Long userId) {
+        StudyTime latestStudyTime = studyTimeRepository.findTopByUserIdOrderByCreatedAtDesc(userId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자의 공부 기록이 없습니다."));
 
+        latestStudyTime.setDeletedAt(LocalDateTime.now());
+        studyTimeRepository.save(latestStudyTime);
     }
 }
