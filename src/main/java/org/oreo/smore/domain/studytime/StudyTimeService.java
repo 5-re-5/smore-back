12d30464c863440595ceeb1af9a5c4ce7238b022
KiminSon <kimin0331@naver.com>
2025-08-05 15:01:11 +0900

    feat: 공부 시작 구현

--- src/main/java/org/oreo/smore/domain/studytime/StudyTimeService.java
+++ src/main/java/org/oreo/smore/domain/studytime/StudyTimeService.java
@@ -3,10 +3,24 @@ package org.oreo.smore.domain.studytime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class StudyTimeService {
-    public void startStudy(Long userId) {
 
+    private final StudyTimeRepository studyTimeRepository;
+
+    /**
+     * 공부 시작
+     */
+    public StudyTime startStudy(Long userId) {
+        StudyTime studyTime = StudyTime.builder()
+                .userId(userId)
+                .createdAt(LocalDateTime.now()) // 서버에서 현재 시간 저장
+                .deletedAt(LocalDateTime.now()) // 서버에서 현재 시간 저장
+                .build();
+
+        return studyTimeRepository.save(studyTime);
     }
 }
