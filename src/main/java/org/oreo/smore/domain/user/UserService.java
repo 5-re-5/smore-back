2e3ce0ec06e0336befacb70064dddcf88ba3d44c
ouvir <kilhyunii@naver.com>
2025-08-07 13:08:08 +0900

    fix: userID 이용 기본 nickname 설정

--- src/main/java/org/oreo/smore/domain/user/UserService.java
+++ src/main/java/org/oreo/smore/domain/user/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -32,12 +33,15 @@ public class UserService {
         User u = User.builder()
                 .name(name)
                 .email(email)
-                .nickname(name + "@" + email)
+                .nickname(UUID.randomUUID().toString())
                 .createdAt(LocalDateTime.now())
                 .goalStudyTime(0)
                 .level("O")
                 .build();
-        return repository.save(u);
+
+        User savedUser = repository.save(u);
+        savedUser.setNickname("OREO" + savedUser.getUserId());
+        return repository.save(savedUser);
     }
 
     @Transactional(readOnly = true)
