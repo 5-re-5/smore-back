461262c43658c568334c40307cabc168910a07a0
hongjungin <jungini0000@gmail.com>
2025-08-08 12:24:33 +0900

    feat: 모든 API 응답을 data 객체로 감싸는 표준화 구현

---
+++ src/main/java/org/oreo/smore/global/common/ApiResponse.java
@@ -0,0 +1,16 @@
+package org.oreo.smore.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private T data;
+
+    public static <T> ApiResponse<T> of(T data) {
+        return new ApiResponse<>(data);
+    }
+}
