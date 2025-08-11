dfdab60d69f8dbff9a11cf846c870155629a153c
KiminSon <kimin0331@naver.com>
2025-08-11 13:06:00 +0900

    feat: 포인트 조회 구현

--- src/main/java/org/oreo/smore/domain/point/PointRepository.java
+++ src/main/java/org/oreo/smore/domain/point/PointRepository.java
@@ -1,6 +1,11 @@
 package org.oreo.smore.domain.point;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PointRepository extends JpaRepository<Point, Long> {
+
+    @Query("select coalesce(sum(p.delta), 0) from Point p where p.userId = :userId")
+    long sumDeltaByUserId(@Param("userId") Long userId);
 }
--- src/main/java/org/oreo/smore/domain/point/dto/response/TotalPointsResponse.java
+++ src/main/java/org/oreo/smore/domain/point/dto/response/TotalPointsResponse.java
@@ -1,4 +1,11 @@
 package org.oreo.smore.domain.point.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class TotalPointsResponse {
+
+    private long totalPoints;
 }
