261d580af747d72d1240be2a718c8c0f77573f7b
KiminSon <kimin0331@naver.com>
2025-08-11 15:48:45 +0900

    feat: 포인트 사용 구현

--- src/main/java/org/oreo/smore/domain/point/dto/response/OreoDrawResponse.java
+++ src/main/java/org/oreo/smore/domain/point/dto/response/OreoDrawResponse.java
@@ -1,4 +1,14 @@
 package org.oreo.smore.domain.point.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class OreoDrawResponse {
+    private String result;
+
+    private String updatedLevel;
+
+    private long updatedPoints;
 }
